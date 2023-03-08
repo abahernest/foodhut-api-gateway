8bd639f7cec6083f30ec01b93fa1dca88b6e01f5
Ernest Abah <abahernesto@gmail.com>
2023-03-08 23:01:23 +0100

    added dispatch and order modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { OrderModule } from './order/order.module';
+import { DispatchModule } from './dispatch/dispatch.module';
 
 @Module({
-  imports: [],
+  imports: [OrderModule, DispatchModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/dispatch/dispatch.module.ts
@@ -0,0 +1,7 @@
+import { Module } from '@nestjs/common';
+import { DispatchController } from './dispatch.controller';
+
+@Module({
+  controllers: [DispatchController]
+})
+export class DispatchModule {}
